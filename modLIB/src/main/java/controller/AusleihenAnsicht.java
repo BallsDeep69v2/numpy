dda22a7f644adb8b6fbe6d15d16d24c09535f8f3
Danièl Baumann <lokamoka100@gmail.com>
2022-05-23 11:13:40 +0200

    all fine 2(should be)

--- modLIB/src/main/java/controller/AusleihenAnsicht.java
+++ modLIB/src/main/java/controller/AusleihenAnsicht.java
@@ -1,12 +1,18 @@
 package controller;
 
+import app.ModLIBStage;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class AusleihenAnsicht implements Initializable {
@@ -43,6 +49,15 @@ public class AusleihenAnsicht implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Stage stage = ModLIBStage.STAGE;
+        backBtn.setOnAction(
+                actionEvent -> {
+                    try {
+                        stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pages/Home.fxml")))));
 
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
--- modLIB/src/main/java/controller/BuchexemplarAnsicht.java
+++ modLIB/src/main/java/controller/BuchexemplarAnsicht.java
@@ -1,19 +1,35 @@
 package controller;
+import app.ModLIBStage;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class BuchexemplarAnsicht implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Stage stage = ModLIBStage.STAGE;
+        backBtn.setOnAction(
+                actionEvent -> {
+                    try {
+                        stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pages/Home.fxml")))));
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 
     @FXML

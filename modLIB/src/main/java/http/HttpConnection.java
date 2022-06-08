5724acc883b9c42ed5e07c6027c4cba4eaccfc92
Danièl Baumann <lokamoka100@gmail.com>
2022-06-08 21:26:43 +0200

    http request for isbn info and json decode (need some fixes - numberformat etc.)

---
+++ modLIB/src/main/java/http/HttpConnection.java
@@ -0,0 +1,73 @@
+package http;
+
+import domain.BuchTyp;
+import errorhandling.ISBNHttpException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class HttpConnection {
+
+
+    private static final String urlLink = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+
+    public static void main(String[] args) throws IOException, ParseException {
+        //9783458317500
+        System.out.println(getBookInfosPerISBN("9783458317500"));
+    }
+
+    public static BuchTyp getBookInfosPerISBN(String isbn) {
+
+        URL url = null;
+        try {
+            url = new URL(urlLink + isbn);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+            String line = null;
+            StringBuilder httpResponse = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                httpResponse.append(line);
+            }
+            br.close();
+            connection.disconnect();
+            System.out.println(httpResponse);
+
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = (JSONObject) parser.parse(httpResponse.toString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            JSONArray items = (JSONArray) jsonObject.get("items");
+            JSONObject itemInfos = (JSONObject) items.get(0);
+            JSONObject volumeInfo = (JSONObject) itemInfos.get("volumeInfo");
+
+            String title = volumeInfo.getOrDefault("title", "").toString();
+            String author = volumeInfo.getOrDefault("authors", "").toString();
+            String description = volumeInfo.getOrDefault("description", "").toString();
+            String genre = volumeInfo.getOrDefault("genre", "").toString();
+            int year, pageCount;
+            try {
+                year = Integer.parseInt(volumeInfo.getOrDefault("publishedDate", "").toString());
+                pageCount = Integer.parseInt(volumeInfo.getOrDefault("pageCount", "").toString());
+            } catch (NumberFormatException ex) {
+                year = 0;
+                pageCount = 0;
+            }
+            return new BuchTyp(isbn, title, author, description, genre, year, pageCount);
+        } catch (IOException e) {
+            throw new ISBNHttpException();
+        }
+    }
+}
